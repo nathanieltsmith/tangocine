dc44dce35284a8f4a5fe4d218ff472be15492493
Nathaniel Smith <nathanieltsmith@gmail.com>
2014-04-30 18:07:15 -0700

    added parsing for song

--- tango_perfs/views.py
+++ tango_perfs/views.py
@@ -343,9 +343,10 @@ def get_orchestras(request):
 	#if request.is_ajax():
 	song = Song.objects.get(simplifiedTitle=unquote(request.GET.get('song', '')))
 	orchestras = []
-	orc_dict = {'title': rec.orchestra.name, 'value': rec.orchestra.ocode}
-	if orc_dict not in orchestras:
-		orchestra.append(orc_dict)
+	for rec in Recording.objects.filter(song=song)
+		orc_dict = {'title': rec.orchestra.name, 'value': rec.orchestra.ocode}
+		if orc_dict not in orchestras:
+			orchestra.append(orc_dict)
 	data = json.dumps(orchestra)
 	#else:
 	#	data = 'fail'
