fbd6771e1211662006bf41dcafc3e2a32267aea6
Nathaniel Smith <nathanieltsmith@gmail.com>
2014-08-07 14:32:45 -0700

    tanda radio playlist tanda lister

--- tango_radio/urls.py
+++ tango_radio/urls.py
@@ -7,8 +7,8 @@ from tango_radio import views
 urlpatterns = patterns('',
     url(r'^tanda/$', views.radio, name='tanda'),
     url(r'^addtanda/$', views.add_tanda, name='add_tanda'),
-    url(r'^api/tanda/$', views.get_tanda, name='get_tanda')
-    url(r'^recordings/$', views.radio_recordings, name='list_recordings')
+    url(r'^api/tanda/$', views.get_tanda, name='get_tanda'),
+    url(r'^recordings/$', views.radio_recordings, name='list_recordings'),
    # url(r'^', views.index, name='index')
 )
 
