3bbc93484170b3a7b3fba82cf2f5b4505cf98ab0
Nathaniel Smith <nathanieltsmith@gmail.com>
2014-08-10 14:21:53 -0700

    added discography page

--- tango_disco/urls.py
+++ tango_disco/urls.py
@@ -6,6 +6,6 @@ from tango_disco import views
 urlpatterns = patterns('',
     url(r'^radio/$', views.radio, name='radio'),
     url(r'^api/update_youtube$', views.radio, name='radio'),
-    url(r'^discography$', views.index, name='discography'))
+    url(r'^discography$', views.index, name='discography')
    # url(r'^', views.index, name='index')
 )
