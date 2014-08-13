fa0c8dc89b2f9284d299c03947f1993c3101563f
Nathaniel Smith <nathanieltsmith@gmail.com>
2014-08-12 23:19:47 -0700

    added singer and date fields to disco search

--- tango_disco/views.py
+++ tango_disco/views.py
@@ -53,11 +53,17 @@ def index(request):
 	genres = [request.POST.get('genre')] if isinstance(request.POST.get('genre'), basestring) else request.POST.get('genre')
 	orchestra_leaders = [request.POST.get('orc')] if isinstance(request.POST.get('orc'), basestring) else request.POST.get('orc')
 	songs = [request.POST.get('song')] if isinstance(request.POST.get('song'), basestring) else request.POST.get('song')
-	start_year = [request.POST.get('start')] if isinstance(request.POST.get('start'), basestring) else request.POST.get('start')
-	end_year = [request.POST.get('end')] if isinstance(request.POST.get('end'), basestring) else request.POST.get('end')
+	singer = [request.POST.get('singer')] if isinstance(request.POST.get('singer'), basestring) else request.POST.get('singer')
+	start_year = [request.POST.get('starting-year')] if isinstance(request.POST.get('starting-year'), basestring) else request.POST.get('starting-year')
+	end_year = [request.POST.get('ending-year')] if isinstance(request.POST.get('ending-year'), basestring) else request.POST.get('ending-year')
 
 	recordings = Recording.objects.select_related('genre__name').filter(recorded__gt='1927-01-01').filter(recorded__lt='1959-01-01').order_by('recorded')
-	
+	if (start_year):
+		recordings = recordings.filter(recorded__gt=start_year[0]+'-01-01')
+	if (end_year):
+		recordings = recordings.filter(recorded__lt=start_year[0]+'-12-31')
+	if (singer):
+		recordings = recordings.filter(singer='singer')
 	if (genres):
 		for genre in genres:
 			if (genre):
