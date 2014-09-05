0c1040a966f02563dda4ec023d02efea13636296
Nathaniel Smith <nathanieltsmith@gmail.com>
2014-09-04 18:17:58 -0700

    added error reporting view

--- tango_disco/urls.py
+++ tango_disco/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     url(r'^radio/$', views.radio, name='radio'),
     url(r'^api/update_youtube$', views.radio, name='radio'),
     url(r'^discography$', views.index, name='discography'),
-    url(r'^api/get_recordings$', views.get_recordings, name='get_recordings')
+    url(r'^api/get_recordings$', views.get_recordings, name='get_recordings'),
+    url(r'^api/report_error$', views.report_error, name='report_error')
+    
    # url(r'^', views.index, name='index')
 )
