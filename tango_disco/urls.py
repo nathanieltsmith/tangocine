8ae4af23a065f85bbd6dcdddd29b808b720e1dc3
Nathaniel Smith <nathanieltsmith@gmail.com>
2014-09-07 17:10:36 -0700

    added superuser inline editing to disco entries

--- tango_disco/urls.py
+++ tango_disco/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     url(r'^discography$', views.index, name='discography'),
     url(r'^api/get_recordings$', views.get_recordings, name='get_recordings'),
     url(r'^api/report_error$', views.report_error, name='report_error'),
-    url(r'^/api/update_recording/(?P<field>.+)/$', views.update_recording, name='update_recording),
+    url(r'^/api/update_recording/(?P<field>.+)/$', views.update_recording, name='update_recording'),
    # url(r'^', views.index, name='index')
 )
